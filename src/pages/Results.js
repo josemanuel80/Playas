14f5851d8816801419dd3e6b28cd72fcc16852e3
Jose Manuel <castellanogonzalezjosemanuel@gmail.com>
2021-08-22 20:12:44 +0200

    corregido

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -6,7 +6,6 @@ import '../styles/Beach.css';
 
 export const Results = () => {
   const [beachToRender, setBeachToRender] = useState([]);
-  const [loading, setloading] = useState(true);
 
   // Obtiene la ciudad por params.
   const { city } = useParams();
@@ -22,7 +21,6 @@ export const Results = () => {
   const allBeaches = async () => {
     const fetchAllBeaches = await getAllBeaches();
     const beachesToShow = getBeaches(fetchAllBeaches, city);
-    setloading(false);
     setBeachToRender(beachesToShow);
   };
 
@@ -33,7 +31,6 @@ export const Results = () => {
   return (
     <>
       <h2>Se han encontrado {beachToRender.length} playas</h2>
-      {loading && <h3>Cargando base de datos.</h3>}
       <div className="main">
         {beachToRender.map((e) => {
           return <Beach key={e.properties.OBJECTID} data={e} />;
